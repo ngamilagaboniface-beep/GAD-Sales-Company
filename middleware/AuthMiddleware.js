42748b9260d478a2998c4f19a1748424d2cec8ce
ngamilagaboniface-beep <ngamilagaboniface@gmail.com>
2026-04-05 17:57:39 +0300

    Create AuthMiddleware for authentication

---
+++ middleware/AuthMiddleware.js
@@ -0,0 +1,8 @@
+class AuthMiddleware {
+    // Method for handling authentication
+    authenticate(req, res, next) {
+        // Authentication logic...
+    }
+}
+
+module.exports = new AuthMiddleware();
